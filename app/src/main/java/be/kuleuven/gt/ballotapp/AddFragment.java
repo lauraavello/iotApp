43e66ac6cf8d28035b923546aa3a961444774304
Laura Avello Caso <laura.avellocaso@student.kuleuven.be>
2025-03-13 16:25:16 +0100

    Add Fragments Created

--- app/src/main/java/be/kuleuven/gt/ballotapp/AddFragment.java
+++ app/src/main/java/be/kuleuven/gt/ballotapp/AddFragment.java
@@ -1,6 +1,40 @@
 package be.kuleuven.gt.ballotapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AddFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        // Find buttons
+        Button btnAddRemark = view.findViewById(R.id.btn_add_remark);
+        Button btnAddAdmin = view.findViewById(R.id.btn_add_admin);
+        Button btnAddBox = view.findViewById(R.id.btn_add_box);
+
+        // Handle clicks to navigate to different fragments
+        btnAddRemark.setOnClickListener(v -> navigateTo(new AddRemarkFragment()));
+        btnAddAdmin.setOnClickListener(v -> navigateTo(new AddAdminFragment()));
+        btnAddBox.setOnClickListener(v -> navigateTo(new AddBoxFragment()));
+
+        return view;
+    }
+
+    private void navigateTo(Fragment fragment) {
+        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout_home, fragment);
+        transaction.addToBackStack(null);  // Allows going back to this fragment
+        transaction.commit();
+    }
 }
---
+++ app/src/main/java/be/kuleuven/gt/ballotapp/AddRemarkFragment.java
@@ -0,0 +1,6 @@
+package be.kuleuven.gt.ballotapp;
+
+import androidx.fragment.app.Fragment;
+
+public class AddRemarkFragment extends Fragment {
+}
